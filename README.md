3d08ad9a69b9360d5ab406c6070e7fcf363f18ec
wildChild <jameswong300@gmail.com>
2016-04-22 09:48:29 -0700

    finish the app after the Udacity review

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
 # FEND-P7-Neighborhood-Map
+It is a user friendly App for displaying the annotation of key locations nearby my living space.
+## How did I built the app
+The structure is similiar to Cat Clicks App: https://github.com/JamesWangbrothers/Cat-Click-App.git
 
-## how I built the app
-The structure is similiar to Cat Clicks App: (https://github.com/JamesWangbrothers/Cat-Click-App.git)
+1. Integrate the google map API https://developers.google.com/maps/documentation/javascript/
+2. Build three major parts: search bar, key locations, map markers
+  * **search bar**: The search has the functino of filtering the model, which decide which part of the model is going to show. when typing words in search bar, key location and marker gets filtered and updated display immediately. Additional feature: auto-completion when search for something.
+  * **key location**: takes the 10 names and items that I have defined in the model, and put them in the list view. additionaly when click the item marker, it highlights the particular item in the map, so you easily find these items in the map
+  * **markers**: when click the selected marker in the map, it pops out a style and a window to show the little description of the item. Third-part API is used here to show the info.
+3. Error handling: adding Error handling for map is not displayed and location is not found
+4. Build the responsive design: 
+    * using the bootstrap mobile-first framework
+    * adding the event listener "resize" for responsive map
+5. Add the CSS styles for animation and display.
+6. optimized the critical rendering path
+7. run HTML validator: https://validator.w3.org/ and CSS validator: http://www.css-validator.org/ to fix errors
+8. submit for feedback and revise
 
-1. integrate google map API
-2. build three major parts: search bar, key locations, map contenting markers for these key locations
-	* search bar
-		The search has the functino of filtering the model, which decide which part of the model is going to show. when typing words in search bar, key location and marker gets filtered and updated display immediately. Additional feature: auto-completion when search for something.
-	* key location
-		takes the 10 names and items that I have defined in the model, and put them in the list view. additionaly when click the item marker, it highlights the particular item in the map, so you easily find these items in the map
-	* markers
-		when click the selected marker in the map, it pops out a style and a window to show the little description of the item. Third-part API is used here to show the info.
-3. build the responsive design
-4. error handling
-5. optimized the critical rendering path
+## how to run the application
+1. Check out the repository or Download or fork the application.
+2. Select index.html to start the app.
+3. Use filter box to filter list items and map markers
+4. Select a list item or map marker to open an info window describing the marker.
 
-## how to use the app
+Additionally,To inspect the site on your phone, you can run a local server
+  ```bash
+  $> cd /path/to/your-project-folder
+  $> python -m SimpleHTTPServer 8080
+  ```
+and Open a browser and visit localhost:8080
+
+OR 
+
+Download and install [ngrok](https://ngrok.com/) to make your local server accessible remotely.
+  ``` bash
+  $> cd /path/to/your-project-folder
+  $> ngrok http 8080
+  ```
+and open a browser and visit localhost:8080
+
+## How to use the application
+The map shows the neighborhood area where I work and live. If you want either want to find out more about myself or the location, please feel free to click the "here" and the bottom of the page and send a email to me to keep in touch!
+Otherwise, please feel free to play with the app and provide me a feedback on how to improve it or what you think about it if you like. The app is easily to play with, you can type anything in the search bar, and the result will be filtered and displayed on the list and map. And if you click he list or the marker on the map, the detail information will pop up! Enjoy and cheer up!
+
+
---
+++ app.js
@@ -0,0 +1,262 @@
+'use strict';
+
+var map,
+    showMapMessage = ko.observable(false);
+//loads the google maps API
+function initMap() {
+    
+    // Create a map object and specify the DOM element for display.
+    var mapOptions =  {
+        center: {lat: 37.368830, lng: -122.03635},
+        scrollwheel: false,
+        zoom: 11,
+        zoomControl: true,
+        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+        //scaleControl: true
+        //streetViewControl: true,
+        //overviewMapControl: true,
+        //rotateControl: true,
+        //disableDefaultUI: true,
+        //mapTpyeId: google.maps.mapTpyeId.ROADMAP,
+        //mapTypeControl: false,
+    };
+
+    var mapDiv = document.getElementById('map');
+    map = new google.maps.Map(mapDiv, mapOptions);
+    
+    ko.applyBindings(new viewModel());
+}
+
+//error message
+function googleError() {
+    showMapMessage(true);
+}
+
+/***********MODEL*************/
+var locations = [
+    {
+        name: "SAP Center",
+        lat: 37.332202,
+        lng: -121.901078
+
+    },
+    {
+        name: "Stanford Univeristy",
+        lat: 37.426441,
+        lng: -122.170973
+
+    },
+    {
+        name: "Mineta San José International Airport",
+        lat: 37.362888,
+        lng: -121.928930
+
+    },
+    {
+        name: "San Jose State Univeristy",
+        lat: 37.334659,
+        lng: -121.880822
+
+    },
+    {
+        name: "California's Great America",
+        lat: 37.392724,
+        lng: -121.964893
+
+    },
+    {
+        name: "Westfield Valley Fair Mall",
+        lat: 37.323682,
+        lng: -121.965667
+
+    },
+    {
+        name: "Levi's Stadium",
+        lat: 37.402035,
+        lng: -121.969099
+
+    },
+    {
+        name: "De Anza College",
+        lat: 37.318669,
+        lng: -122.046003
+
+    },
+    {
+        name: "Computer History Museum",
+        lat: 37.413713,
+        lng: -122.077203
+
+    },
+    {
+        name: "Googleplex",
+        lat: 37.421732,
+        lng: -122.083876
+
+    }   
+];
+
+/***********CONSTRUCTOR*************/
+var Place = function(data) {
+    var self = this;
+    self.name = ko.observable(data.name);
+    self.lat = ko.observable(data.lat);
+    self.lng = ko.observable(data.lng);
+    self.description = ko.observable(data.description);
+    self.url = ko.observable('');
+    self.marker = ko.observable();
+    self.address = ko.observable('');
+    self.contentString = ko.observable('');
+
+};
+
+/***********VIEW MODEL*************/
+var viewModel = function(){
+
+    var self = this;
+
+    //another way to do filtering search
+    //self.places = ko.observableArray(locations);
+
+    // self.filter = ko.observable('');
+
+    // self.search = ko.computed(function(){
+    //     return ko.utils.arrayFilter(self.places(), function(point){
+    //         return point.name.toLowerCase().indexOf(self.filter().toLowerCase()) >= 0;
+    //     });
+    // });
+
+    var client_id = '3YUNRB2OBRZFDK3NDXIKNGHQJOPD0JKSZ4P0MBFQVXTA33RN',
+        client_secret = '3EN00YN11KGVKLNGT5WFRKCXN5VZ2J4PSEZGJTA4CRUWFJEM',
+        infowindow = new google.maps.InfoWindow({maxWidth:200}),
+        image = 'Images/mapicon.png',
+        searchInput,
+        location,
+        marker,
+        url,
+        venue;
+
+    //array of places
+    self.places = ko.observableArray([]);
+
+    //foursqaure error ko
+    self.showMessage = ko.observable(false);
+
+    //call the constructor
+    locations.forEach(function(placeItem){
+        self.places.push(new Place(placeItem));
+    });
+
+    //set markers, request foursquare data and set listeners
+    self.places().forEach(function(placeItem){
+
+        //define markers
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(placeItem.lat(), placeItem.lng()),
+            map: map,
+            icon: image,
+            animation: google.maps.Animation.DROP
+        });
+        placeItem.marker = marker;
+
+        /***********FOURSQUARE*************/
+        // If data call is successful - check for various properties and assign them to observables
+        // Alert the user on error
+        var request = $.ajax({
+            url:'https://api.foursquare.com/v2/venues/search',
+            dataType: 'json',
+            data: 'limit=1' +
+                    '&ll=37.370626,-122.012272' +
+                    '&query=' + placeItem.name() +
+                    '&client_id='+ client_id +
+                    '&client_secret='+ client_secret +
+                    '&v=20160419',
+        }).done(function (data) { 
+            // If incoming data has a venues object set the first one to the var venue
+            venue = data.response.hasOwnProperty("venues") ? data.response.venues[0] : '';
+
+            // If the new venue has a property called location set that to the variable location
+            location = venue.hasOwnProperty('location') ? venue.location : '';
+                // If new location has prop address then set the observable address to that or blank
+                if (location.hasOwnProperty('address')) {
+                    placeItem.address(location.address || '');
+                }
+
+            url = venue.hasOwnProperty('url') ? venue.url : '';
+                placeItem.url(url || '');
+
+            // Content of the infowindow
+            placeItem.contentString = '<div id="iWindow"><h5>' + placeItem.name() + '</h5>'
+                    +'<p>' + placeItem.address() + '</p><p><a href=' + placeItem.url() + '>' + placeItem.url() +
+                    '</a></p><p><a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' +
+                    placeItem.lat() + ',' + placeItem.lng() + '>Directions</a></p></div>';
+
+            // Add infowindows
+            google.maps.event.addListener(placeItem.marker, 'click', function () {
+                infowindow.open(map, this);
+                // Bounce animation
+                placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function () {
+                    placeItem.marker.setAnimation(null);
+                }, 1400);
+                infowindow.setContent(placeItem.contentString);
+
+                // 3 seconds after the center of the map has changed, pan back to the marker.
+                window.setTimeout(function() {
+                    map.panTo(marker.getPosition());
+                }, 3000);
+
+            });
+        }).fail(function (e) {
+            infowindow.setContent('<h5>Foursquare data is unavailable.</h5>');
+            // self.showMessage(true);
+        });
+        
+
+        //add event listener for responsive map
+        google.maps.event.addDomListener(window, 'resize', function() {
+            var center = map.getCenter();
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(center); 
+        });
+        
+    });
+
+    // Show the marker when the user clicks the list
+    self.showInfo = function (placeItem) {
+        google.maps.event.trigger(placeItem.marker, 'click');
+    };
+
+    // Array containing markers based on search
+    self.visible = ko.observableArray();
+
+    // All markers are visible by default
+    self.places().forEach(function (place) {
+        self.visible.push(place);
+    });
+    
+    //build the filter function to display search result, if input matches leave marker
+
+    // Track input
+    self.userInput = ko.observable('');
+
+    self.filterMarkers = function () {
+        // Set all markers and places to not visible.
+        searchInput = self.userInput().toLowerCase();
+        //close current infowindows when search term entered
+        infowindow.close();
+        self.visible.removeAll();
+        
+        self.places().forEach(function (place) {
+            place.marker.setVisible(false);
+                // If user input is included in the name, set marker as visible
+                if (place.name().toLowerCase().indexOf(searchInput) !== -1) {
+                self.visible.push(place);
+            }
+        });
+        
+        self.visible().forEach(function (place) {
+            place.marker.setVisible(true);
+        });
+    };
+};
